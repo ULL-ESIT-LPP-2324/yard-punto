8134a626dcbe404fc7f1309ce8fd44e5daa3c947
Coromoto León <cleon@ull.edu.es>
2023-10-30 11:26:56 +0000

    NoMethodError: undefined method `x'
    rspec -I. spec/punto_spec.rb
    .F
    
    Failures:
    
      1) Punto Inicialización de un punto Se cuenta con métodos getters para las coordenadas x e y
         Failure/Error: expect(Punto.new(0,0).x).to eq(0)
    
         NoMethodError:
           undefined method `x' for #<Punto:0x0000562dccfca918 @x=0, @y=0>
         # ./spec/punto_spec.rb:9:in `block (3 levels) in <top (required)>'
    
    Finished in 0.00633 seconds (files took 0.14975 seconds to load)
    2 examples, 1 failure
    
    Failed examples:
    
    rspec ./spec/punto_spec.rb:8 # Punto Inicialización de un punto Se cuenta con métodos getters para las coordenadas x e y

--- spec/punto_spec.rb
+++ spec/punto_spec.rb
@@ -5,5 +5,9 @@ describe Punto do
     it "Un punto cuenta con dos cordenadas la x y la y" do
       expect(Punto.new(0,0)).not_to eq(nil)
     end
+    it "Se cuenta con métodos getters para las coordenadas x e y" do
+      expect(Punto.new(0,0).x).to eq(0)
+      expect(Punto.new(0,0).y).to eq(0)
+    end
   end
 end
