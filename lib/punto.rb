6e7bf1daf4ae542252c4a9e9732dd5a8644073e2
Coromoto León <cleon@ull.edu.es>
2023-11-03 11:31:32 +0000

    añadidas nuevas funcionalidades

--- lib/punto.rb
+++ lib/punto.rb
@@ -1,3 +1,5 @@
+# Clase para representar un punto en un módulo de Figuras
+# Se ha de instanciar con dos argumentos x e y
 class Punto
   attr_reader :x, :y
   def initialize(x,y)
@@ -6,4 +8,13 @@ class Punto
   def to_s
     "(#{@x},#{@y})"
   end
+
+  # método de instancia para sumar dos puntos
+  def + (otro)
+     Punto.new(x+ otro.x, y+otro.y)
+  end
+
+  #constantes - el origen
+  ORIGEN::Punto.new(0,0)
+  UNIDAD::Punto.new(1,1)
 end
