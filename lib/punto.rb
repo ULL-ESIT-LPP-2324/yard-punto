3610fa596109cd5cbe492fb76bd9045d3ba6b3b9
Coromoto León <cleon@ull.edu.es>
2023-11-08 09:08:50 +0000

    yard doc --private

--- lib/punto.rb
+++ lib/punto.rb
@@ -1,8 +1,26 @@
+# Clase para representar un punto
+#
+# @author Coromoto León
+# @copyright Copyright (c) 2023 LPP
+# @licence Distributes under the same terms as Ruby
+#
+# Esta clase es un ejemplo utilizado en las clases
 class Punto
-  attr_reader :x, :y
+  # metodo para obtener el valor de la coordenada x
+  attr_reader :x
+  # metodo para obtener el valor de la coordenada x
+  attr_reader :y
+
+  # método para la creación de un objeto punto
+  # @param x, y [Numeric]
+  # @retun un objecto
   def initialize(x,y)
     @x, @y = x, y
   end
+
+  #metodo para convertir en una cadena un punto
+  # @param sin parámetros 
+  # @retun una cadena [String] 
   def to_s
     "(#{@x},#{@y})"
   end
